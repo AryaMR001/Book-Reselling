1adbbe2156703abaf30cb2a96a394c6961d1516b
AryaMR001 <36833983+AryaMR001@users.noreply.github.com>
2019-03-28 18:28:16 +0530

    Browse book is modified with no found and committed

--- Browse_Book.aspx.cs
+++ Browse_Book.aspx.cs
@@ -12,7 +12,19 @@ public partial class Browse_Book : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         cn.start();
-        DataList1.DataSource = cn.fill("select Bok_Name,Author_Name,Edition,Category,Publication,Price,Condition,Page_NO,Book_Image from Book_Data where Bok_Name like '" + Session["search"].ToString() + "%' or Author_Name like '" + Session["search"].ToString() + "%' or Condition like '" + Session["search"].ToString() + "%'");
+        DataList1.DataSource = cn.fill("select Bok_Name,Author_Name,Edition,Category,Publication,Price,Condition,Page_NO,Book_Image from Book_Data where Bok_Name like '" + Session["search"].ToString() + "%' or Author_Name like '" + Session["search"].ToString() + "%' or Condition like '" + Session["search"].ToString() + "%' or Publication like '" + Session["search"].ToString() + "%'");
         DataList1.DataBind();
+        if (DataList1 == null || DataList1.Items.Count == 0)
+        {
+            Label2.Visible = true;
+            Label2.Text = " Sorry this book is not available...You can requset wanted book to sellers";
+            
+        }
+           
+    }
+
+    protected void DataList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
     }
 }
