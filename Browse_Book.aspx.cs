1c5bd6a37df6666766d8569561244ab2e71e1c15
AryaMR001 <36833983+AryaMR001@users.noreply.github.com>
2019-03-28 17:56:20 +0530

    browse book code committed(28-03-19)

---
+++ Browse_Book.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+public partial class Browse_Book : System.Web.UI.Page
+{
+    Connect cn = new Connect();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        cn.start();
+        DataList1.DataSource = cn.fill("select Bok_Name,Author_Name,Edition,Category,Publication,Price,Condition,Page_NO,Book_Image from Book_Data where Bok_Name like '" + Session["search"].ToString() + "%' or Author_Name like '" + Session["search"].ToString() + "%' or Condition like '" + Session["search"].ToString() + "%'");
+        DataList1.DataBind();
+    }
+}
