9b7934725650fcafa96d2015afa7bce2450b707d
AryaMR001 <36833983+AryaMR001@users.noreply.github.com>
2019-04-09 16:33:41 +0530

    view request from buyer

---
+++ ViewRequest.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+public partial class ViewRequest : System.Web.UI.Page
+{
+    Connect cn = new Connect();
+    int count;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        cn.start();
+        Label1.Visible = true;
+        cn.dr = cn.read("select count(Book_Name) from Book_Request where status IS NULL ");
+        if(cn.dr.Read())
+        {
+            count = Convert.ToInt32(cn.dr.GetValue(0).ToString());
+        }
+        cn.dr.Close();
+        
+        GridView1.DataSource = cn.fill("select Book_Name,Author,Edition from Book_Request where status IS NULL");
+        GridView1.DataBind();
+        if (GridView1 == null)
+        {
+            Label1.Text = "You Have No Notification";
+        }
+        else
+        {
+            Label1.Text = "You Have"+" "+count+" "+"Notifications";
+        }
+    }
+
+    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        
+    }
+}
