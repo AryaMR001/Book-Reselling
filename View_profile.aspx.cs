1d60bcf0b1c1f715cb4b1f84d0a2ef278676afad
AryaMR001 <36833983+AryaMR001@users.noreply.github.com>
2019-04-09 16:34:29 +0530

    view profile

---
+++ View_profile.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+public partial class View_profile : System.Web.UI.Page
+{
+    Connect cn = new Connect();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        cn.start();
+      
+        DetailsView1.DataSource = cn.fill("select Name,Gender,Address,Phnnumber,State,District,Email_ID from Sign_UP where Reg_ID='"+ Session["Reg_ID"].ToString() + "' ");
+        DetailsView1.DataBind();
+    }
+
+    protected void DetailsView1_PageIndexChanging(object sender, DetailsViewPageEventArgs e)
+    {
+
+    }
+}
