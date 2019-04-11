8d71f798d93623e7b86430a45b8f3a1502642ff9
AryaMR001 <36833983+AryaMR001@users.noreply.github.com>
2019-04-11 15:57:42 +0530

    View shopping cart without count code

---
+++ View_ShoppingCart.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class View_ShoppingCart : System.Web.UI.Page
+{
+    Connect cn = new Connect();
+   
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        cn.start();
+        GridView1.DataSource = cn.fill("select b.Bok_Name,b.Author_Name,b.Edition,b.Category,b.Publication,b.Price,b.Book_Image from Book_Data b,Add_Cart a where b.Book_ID=a.Book_ID");
+        GridView1.DataBind();
+    }
+
+    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+       
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+    }
+}
