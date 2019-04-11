af55e12d2ff126a7da7cc961f41cce3a8ee77c73
AryaMR001 <36833983+AryaMR001@users.noreply.github.com>
2019-04-11 12:41:31 +0530

    Home page is changed with add to cart

--- Home.aspx.cs
+++ Home.aspx.cs
@@ -4,12 +4,41 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data;
+using System.Data.SqlClient;
 public partial class Home : System.Web.UI.Page
 {
+    Connect cn = new Connect();
+    int bid, count;
     protected void Page_Load(object sender, EventArgs e)
     {
+        cn.start();
+        if (!IsPostBack)
+        {
+            DataList1.DataSource = cn.fill("select top 12 Bok_Name,Book_ID,Book_Image,Price from Book_Data");
+            DataList1.DataBind();
+        }
+            
         
        
+    }
+    protected void AddCart(object source, DataListCommandEventArgs e)
+    {
+
+        if (e.CommandName == "Add")
+        {
+            bid = Convert.ToInt32(e.CommandArgument.ToString());
+            Session["BID"] = bid.ToString();
+            cn.dml("insert into Add_Cart(Book_ID,Reg_ID)values('" + Session["BID"].ToString() + "','" + Session["Reg_ID"].ToString() + "')");
+            cn.dr = cn.read("select count(Book_ID) from Add_Cart where Reg_ID='" + Session["Reg_ID"].ToString() + "'");
+            if (cn.dr.Read())
+            {
+                count = Convert.ToInt32(cn.dr.GetValue(0).ToString());
+
+
+            }
+            txtcount.Text = count + "" + "Book(s)";
+            count = count + 1;
+        }
     }
 }
