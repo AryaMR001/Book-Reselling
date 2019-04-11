aeface724280a681452ae092ab97c7889ca3280a
AryaMR001 <36833983+AryaMR001@users.noreply.github.com>
2019-04-11 12:39:25 +0530

    changes made with add to cart

--- NewBook.aspx.cs
+++ NewBook.aspx.cs
@@ -12,16 +12,43 @@ using System.Data.SqlClient;
 public partial class NewBook : System.Web.UI.Page
 {
     Connect cn = new Connect();
-
+    int bid, count;
     protected void Page_Load(object sender, EventArgs e)
     {
         cn.start();
-        DataList1.DataSource=cn.fill("select Bok_Name,Book_Image,Price from Book_Data");
-        DataList1.DataBind();
+        if (!IsPostBack)
+        {
+            DataList1.DataSource = cn.fill("select Bok_Name,Book_ID,Book_Image,Price from Book_Data");
+            DataList1.DataBind();
+        }
+      
+      
+        
     }
 
     protected void DataList1_SelectedIndexChanged(object sender, EventArgs e)
     {
+      
+    }
 
+
+    protected void AddCart(object source, DataListCommandEventArgs e)
+    {
+      
+        if (e.CommandName=="Add")
+        {
+            bid = Convert.ToInt32(e.CommandArgument.ToString());
+            Session["BID"] = bid.ToString();
+            cn.dml("insert into Add_Cart(Book_ID,Reg_ID)values('" + Session["BID"].ToString() + "','" + Session["Reg_ID"].ToString() + "')");
+            cn.dr = cn.read("select count(Book_ID) from Add_Cart where Reg_ID='" + Session["Reg_ID"].ToString() + "'");
+            if(cn.dr.Read())
+            {
+                count= Convert.ToInt32(cn.dr.GetValue(0).ToString());
+               
+                
+            }
+            txtcount.Text = count + "" + "Book(s)";
+            count = count + 1;
+        }
     }
 }
