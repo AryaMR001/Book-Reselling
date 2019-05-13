98c10a66d8ec1fb1b6d1632eb91e1907bbf41498
AryaMR001 <36833983+AryaMR001@users.noreply.github.com>
2019-05-13 10:47:59 +0530

    Add files via upload

---
+++ Billing.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Billing : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+    }
+
+    protected void btnpay_Click(object sender, EventArgs e)
+    {
+
+    }
+}
