f81cb90e6ed5d4b12bfd7c2078e4f33d286d8fbc
AryaMR001 <36833983+AryaMR001@users.noreply.github.com>
2019-05-14 15:01:20 +0530

    Add files via upload

---
+++ Best-selling-book.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+public partial class Best_selling_book : System.Web.UI.Page
+{
+    Connect cn = new Connect();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+        cn.start();
+        if (!IsPostBack)
+        {
+            PopulateReport();
+        }
+    }
+    private void PopulateReport()
+    {
+        Chart1.DataSource = cn.fill("select b.Bok_Name,count(a.Book_ID) as count from Book_Data b,Add_Cart a where a.Book_ID=b.Book_ID  group by b.Bok_Name order by count(a.Book_ID) DESC");
+        Chart1.DataBind();
+    }
+
+
+
+    protected void Chart1_Load(object sender, EventArgs e)
+    {
+
+    }
+}
